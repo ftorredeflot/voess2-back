c6f10ad9420addd69f6498df2f911c87cf7e0d61
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-03-10 18:54:41 +0100

    Añadidos nombre i apellidos al registro, arreglado el control de errores que fallavan y retoques de estilo pequeños #17

--- src/main/webapp/app/account/register/register.state.js
+++ src/main/webapp/app/account/register/register.state.js
@@ -24,6 +24,7 @@
             },
             resolve: {
                 translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('user-management');
                     $translatePartialLoader.addPart('userExt');
                     $translatePartialLoader.addPart('sexGender');
                     $translatePartialLoader.addPart('register');
