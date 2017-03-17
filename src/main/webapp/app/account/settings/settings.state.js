051d6805fad84213058e069f13a19ffcec72d95b
danylprokofyev <danyl.prokofyev@gmail.com>
2017-03-17 18:12:40 +0100

    #35 settings/ajustes de usuario WIP

--- src/main/webapp/app/account/settings/settings.state.js
+++ src/main/webapp/app/account/settings/settings.state.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -25,6 +25,9 @@
             resolve: {
                 translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                     $translatePartialLoader.addPart('settings');
+                    $translatePartialLoader.addPart('user-management');
+                    $translatePartialLoader.addPart('userExt');
+                    $translatePartialLoader.addPart('sexGender');
                     return $translate.refresh();
                 }]
             }
