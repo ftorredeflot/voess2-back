f0588baad36abd164b6b8812e24fd1b024b1cd6a
kelvinlt <kelvinleethen@gmail.com>
2017-04-03 16:56:43 +0200

    He creado un state para tournamentss. Basico de prueba. El boton de navbar de tournaments ira a /tournaments.

---
+++ src/main/webapp/app/tournaments/tournaments.state.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('voess2App')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('tournaments', {
+            parent: 'app',
+            url: '/tournaments',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/tournaments/tournaments.html',
+                    controller: 'HomeControler',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
