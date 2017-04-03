21a062fd44034aac0dae69860621e55c24856b9c
kelvinlt <kelvinleethen@gmail.com>
2017-04-03 20:02:52 +0200

    He creado un state para league. Basico de prueba. El boton de navbar de database ira a /game/league.
    #56

---
+++ src/main/webapp/app/games/league/league.state.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('voess2App')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('league', {
+            parent: 'app',
+            url: '/games/league',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/games/league/league.html',
+                    controller: 'HomeControler',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
