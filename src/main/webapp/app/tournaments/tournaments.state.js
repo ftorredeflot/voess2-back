98061780c8c0d730e740d56c2566c1b0020d6928
danylprokofyev <danyl.prokofyev@gmail.com>
2017-04-03 21:07:14 +0200

    #52 enseñar TODO enseñar bonito

--- src/main/webapp/app/tournaments/tournaments.state.js
+++ src/main/webapp/app/tournaments/tournaments.state.js
@@ -1,4 +1,34 @@
-(function() {
+// (function() {
+//     'use strict';
+//
+//     angular
+//         .module('voess2App')
+//         .config(stateConfig);
+//
+//     stateConfig.$inject = ['$stateProvider'];
+//
+//     function stateConfig($stateProvider) {
+//         $stateProvider.state('tournaments', {
+//             parent: 'app',
+//             url: '/tournaments',
+//             data: $stateProvider,
+//             views: {
+//                 'content@': {
+//                     templateUrl: 'app/tournaments/tournaments.html',
+//                     controller: 'TournamentVoessController',
+//                     controllerAs: 'vm'
+//                 }
+//             },
+//             resolve: {
+//                 mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+//                     $translatePartialLoader.addPart('home');
+//                     return $translate.refresh();
+//                 }]
+//             }
+//         });
+//     }
+// })();
+(function () {
     'use strict';
 
     angular
@@ -8,25 +38,26 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('tournaments', {
-            parent: 'app',
-            url: '/tournaments',
-            data: {
-                authorities: []
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/tournaments/tournaments.html',
-                    controller: 'HomeControler',
-                    controllerAs: 'vm'
+        $stateProvider
+            .state('AllTournamentVoessController', {
+                parent: 'app',
+                url: '/tournaments',
+                views: {
+                    'content@': {
+                        templateUrl: 'app/tournaments/tournaments.html',
+                        controller: 'TournamentVoessController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('tournament');
+                        $translatePartialLoader.addPart('global');
+                        $translatePartialLoader.addPart('home');
+                        return $translate.refresh();
+                    }]
                 }
-            },
-            resolve: {
-                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
-                    $translatePartialLoader.addPart('home');
-                    return $translate.refresh();
-                }]
-            }
-        });
+            })
     }
+
 })();
