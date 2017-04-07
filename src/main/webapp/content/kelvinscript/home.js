a9d4b65f6d6e4146d74168eaa132966202746913
kelvinlt <kelvinleethen@gmail.com>
2017-04-07 17:16:29 +0200

    Quitando una cosa de prueba del navbar

--- src/main/webapp/content/kelvinscript/home.js
+++ src/main/webapp/content/kelvinscript/home.js
@@ -4,7 +4,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 11, 2017 21:15:00").getTime();
+var countDownDate = new Date("Apr 11, 2017 21:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
